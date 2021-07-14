23ff2b62640db34843b3e46c93b409aef080c33e
travelbeeee <sochun1518@naver.com>
2021-07-14 12:59:57 +0900

    예외처리공부

---
+++ 스프링MVC2편/exception/src/main/java/travelbeeee/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,35 @@
+package travelbeeee.exception.exhandler.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import travelbeeee.exception.exception.UserException;
+import travelbeeee.exception.exhandler.ErrorResult;
+
+@Slf4j
+@RestControllerAdvice
+public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[ExceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e){
+        log.error("[ExceptionHandler] ex", e);
+        return new ResponseEntity<>(new ErrorResult("USER-EX", e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[ExceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+}
