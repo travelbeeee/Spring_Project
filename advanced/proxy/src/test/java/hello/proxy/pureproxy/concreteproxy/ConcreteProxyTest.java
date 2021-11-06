5710403a7342b612e70d870286d174a4c5a1b734
travelbeeee <sochun1518@naver.com>
2021-11-06 20:00:04 +0900

    프록시패턴공부마무리

---
+++ advanced/proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -0,0 +1,28 @@
+package hello.proxy.pureproxy.concreteproxy;
+
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
+import org.junit.jupiter.api.Test;
+
+public class ConcreteProxyTest {
+
+    @Test
+    void noPorxy(){
+        //given
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    void addProxy(){
+        //given
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        //when
+        client.execute();
+        //then
+    }
+}
